e6f6ab910f7fdce6944d402fb3ad65a99f237acb
rahmasayed <36827038+rahmasayed@users.noreply.github.com>
2025-11-20 21:46:57 +0200

    Merged BasePage.java

--- Implementation (Automation Testing)/AutomationTesting/src/main/java/pages/BasePage.java
+++ Implementation (Automation Testing)/AutomationTesting/src/main/java/pages/BasePage.java
@@ -2,15 +2,26 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class BasePage {
+
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        
+        // Create explicit wait available for all pages
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         PageFactory.initElements(driver, this);
     }
 
+    // --- mahfouz methods 
     public String getPageTitle() {
         return driver.getTitle();
     }
@@ -19,4 +30,5 @@ public class BasePage {
         return driver.getCurrentUrl();
     }
 }
+
 
