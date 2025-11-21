861f8db0b044856c6ccf9f8ca222e2a26601215c
rahmasayed <36827038+rahmasayed@users.noreply.github.com>
2025-11-21 12:46:26 +0200

    VerifyQuantityTest.java

--- Implementation (Automation Testing)/AutomationTesting/src/test/java/tests/VerifyQuantityTest.java
+++ Implementation (Automation Testing)/AutomationTesting/src/test/java/tests/VerifyQuantityTest.java
@@ -4,40 +4,36 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CartPage;
 import pages.HomePage;
+import pages.ProductDetailsPage;
 import pages.ProductsPage;
 
 public class VerifyQuantityTest extends BaseTest {
     HomePage homePage;
     ProductsPage productsPage;
+    ProductDetailsPage productDetailsPage;
     CartPage cartPage;
 
     @Test(priority = 2)
-    public void testVerifyProductQuantity() {
+    public void testVerifyProductQuantityFromDetails() {
+   
         homePage = new HomePage(driver);
         productsPage = new ProductsPage(driver);
+        productDetailsPage = new ProductDetailsPage(driver);
         cartPage = new CartPage(driver);
 
         homePage.openHomePage();
-        homePage.openProductsPage();
+        productsPage = homePage.openProductsPage();
 
-// Add first product
-        productsPage.addProductToCartByIndex(0);
+        productsPage.openProductByIndex(0);
 
-        productsPage.clickContinueShoppingFromPopup(10);
+        // Set quantity to 4
+        productDetailsPage.setQuantity(4);
+        productDetailsPage.clickAddToCart();
+        productDetailsPage.clickViewCartFromPopup(10);
+        boolean rowsExist = cartPage.waitForCartRows(10);
+        Assert.assertTrue(rowsExist, "Cart page is empty after adding product.");
 
-        productsPage.waitForPopupToDisappear(5);
-
-// Add second product
-        productsPage.addProductToCartByIndex(1);
-
-
-        productsPage.clickViewCartFromPopup(10);
-
-        int quantityFirst = cartPage.getQuantityForIndex(0);
-        int quantitySecond = cartPage.getQuantityForIndex(1);
-
-        Assert.assertEquals(quantityFirst, 1, "First product quantity mismatch.");
-        Assert.assertEquals(quantitySecond, 1, "Second product quantity mismatch.");
+        int quantityInCart = cartPage.getQuantityForIndex(0);
+        Assert.assertEquals(quantityInCart, 4, "Product quantity in cart does not match the quantity added from product details page.");
     }
-
 }
